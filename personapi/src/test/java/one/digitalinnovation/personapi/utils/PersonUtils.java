c1e1b5f53ba0e621b5c8354bb5878892e6c807c8
MariaLuizautfpr2021 <93231614+MariaLuizautfpr2021@users.noreply.github.com>
2022-12-07 21:50:00 -0300

    Update PersonUtils.java

--- personapi/src/test/java/one/digitalinnovation/personapi/utils/PersonUtils.java
+++ personapi/src/test/java/one/digitalinnovation/personapi/utils/PersonUtils.java
@@ -7,11 +7,11 @@ import java.time.LocalDate;
 
 public class PersonUtils {
 
-    private static final String FIRST_NAME = "Mylena";
-    private static final String LAST_NAME = "Furlan";
-    private static final String CPF_NUMBER = "001.002.003-20";
-    private static final String END_NAME = "Rua Lala";
-    private static final String MUNICIPIO_NAME = "Cocoricó";
+    private static final String FIRST_NAME = "Luiza";
+    private static final String LAST_NAME = "Souza";
+    private static final String CPF_NUMBER = "099.865.179-66";
+    private static final String END_NAME = "Rua Rubi";
+    private static final String MUNICIPIO_NAME = "Cornelio Procopio";
     private static final String PESSOA_NAME = "PJ";
     private static final long PERSON_ID = 1L;
     public static final LocalDate BIRTH_DATE = LocalDate.of(2022, 10, 1);
